b238a206b82e2c270dc8a23a6fae2f4f6d6a13c5
David Song <David.Song0802@gmail.com>
2015-01-14 13:12:59 -0500

    info-page demo

--- ConNextor/scripts/outpost.js
+++ ConNextor/scripts/outpost.js
@@ -1,4 +1,21 @@
 function postEmailToServer() {
     var emailAddress = document.getElementById("emailInput").value;
-    console.log('dummy! ' + emailAddress);
+
+    // do post
+    $.get("http://135.0.25.246:2999", {email: emailAddress}, function (data, status) {
+        if (status !== 'success') {
+            alert("Connection Refused, Please try again.");
+        } else if (data === 'true') {
+            alert("Email saved");
+        } else if (data === 'false') {
+            alert("Email failed to save");
+        }
+    });
 }
+
+
+$(".input-upright").keyup(function (e) {
+    if (e.keyCode == 13) {
+        postEmailToServer()
+    }
+});

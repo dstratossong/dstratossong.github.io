2ac32cadd7748a9766839f1c709e0282b7886f57
David Song <David.Song0802@gmail.com>
2015-01-14 00:38:17 -0500

    info-page demo

--- ConNextor/scripts/scroll.js
+++ ConNextor/scripts/scroll.js
@@ -1,19 +1,12 @@
-var numOfSections = 5;
-//var adjustment = 120;
-//var section = $(document).height() / numOfSections;
-//var trackSection = 0;
-var fadeTime = 500;
-//var seen=[numOfSections];
-
-var lightBulbVisible = false;
-var currentSection,
+var lightBulbVisible = false,
+	currentSection,
 	currentDisplayingPicture = 0,
-	offset = 0.5;
+	offset = 0.5,
+	fadeTime = 300;
 
-//$(document).ready(function(){
-//	start();
-//});
-
+/**
+ * With every scroll, checks the current position and the section updates.
+ */
 $(window).scroll(function () {
 	var distanceFromTop = $(window).scrollTop(),
 		windowHeight = $(window).height();
@@ -26,43 +19,35 @@ $(window).scroll(function () {
 		lightBulbVisible = true;
 	}
 
+	// note that we round the current section anyways
+	// any offset < 0.5 would cause periods of inactivity
+	// any offset > 0.5 would cause this function to be called too many times
 	if (Math.abs(currentSection - currentDisplayingPicture) > offset) {
 		changeSection(Math.round(currentSection));
 	}
-
-   //if($(window).scrollTop() > ($(window).height())*(trackSection)-adjustment) {
-	// console.log("changed!");
-	// changeSection();
-	// trackSection+=1;
-   //}
-   //else{
-	//   if (($(window).height())*(trackSection)-$(window).scrollTop()>$(window).height()*1.5){
-	//		console.log("up!");
-	//		trackSection-=2;
-	//		if (trackSection<1){
-	//			trackSection=1;
-	//		}
-	//		changeSection();
-	//	}
-	//}
 });
 
-function changeSection(newPicture) {
-	if (currentDisplayingPicture == newPicture) return;
-	console.log('changed to ' + newPicture);
+/**
+ * Updates all visual elements associated with the given section.
+ * This includes the time-line side pane, the picture in the bulb
+ * as well as the background change and dotted line
+ *
+ * @param newSection
+ */
+function changeSection(newSection) {
+	if (currentDisplayingPicture == newSection) return;
+	console.log('changed to ' + newSection);
 
 	//if (currentDisplayingPicture) { // is not zero
 	//	document.getElementsByClassName()
 	//}
 
-	document.getElementById("light_bulb_image").className = "light_bulb_image"+(newPicture);
+	document.getElementById("light_bulb_image").className = "light_bulb_image"+(newSection);
 	$("#light_bulb_image_container").hide().fadeIn(fadeTime);
-	currentDisplayingPicture = newPicture;
+	$("#section"+newSection+"text").fadeIn(fadeTime);
+	currentDisplayingPicture = newSection;
 }
 
-//function start(){
-//	for (i = 0; i < seen.length; i++) {
-//		seen[i]=false;
-//	}
-//	trackSection=1;
-//}
+function highlightCurrentTimeLineElement(newSection) {
+
+}

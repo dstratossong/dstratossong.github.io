ea82b05e4ab4ddd025ee6801eb73fe288343d6cb
David Song <david.song0802@gmail.com>
2015-10-27 21:54:31 -0400

    added working nav bar

--- scripts/base.js
+++ scripts/base.js
@@ -1,11 +1,10 @@
 function navScrollHook() {
   var distFromTop = $(window).scrollTop();
-  var viewPortSize = $(window).height();
 
   var nav = $('.nav');
   var anchor = $('#nav');
 
-  if (distFromTop >= anchor.offset().top - viewPortSize) {
+  if (distFromTop >= anchor.offset().top) {
     nav.addClass('nav-fixed-top');
   } else {
     nav.removeClass('nav-fixed-top');

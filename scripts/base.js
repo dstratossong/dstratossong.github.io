ea912203325fe1882dfab31ec24662856071100f
David Song <david.song0802@gmail.com>
2015-10-30 22:40:22 -0400

    finished active nav

--- scripts/base.js
+++ scripts/base.js
@@ -1,22 +1,47 @@
+
+var navHeight = 50;
+var scrollPadding = 20;
+var totalOffset = navHeight + scrollPadding;
+
 function navScrollHook() {
-  var distFromTop = $(window).scrollTop();
+
+  var windowOffset = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  var documentHeight = $(document).height();
 
   var nav = $('.nav');
   var anchor = $('#nav');
 
-  if (distFromTop >= anchor.offset().top) {
+  if (windowOffset >= anchor.offset().top) {
     nav.addClass('nav-fixed-top');
   } else {
     nav.removeClass('nav-fixed-top');
   }
 
-  $(".nav-section").each(function () {
-    var sectionId = $(this).attr("id");
-    if ($(this).visible(true)) {
-      $("#" + sectionId + "-menu").addClass("active");
-    } else {
-      $("#" + sectionId + "-menu").removeClass("active");
+  var $toBeActive = null;
+
+  $("section").each(function () {
+    var $section = $(this);
+    var sectionId = $section.attr("id");
+    var sectionHeight = $section.height();
+    var sectionOffset = $section.offset().top;
+
+    if ($toBeActive == null) {
+
+      if (sectionOffset - totalOffset <= windowOffset && 
+          sectionOffset + sectionHeight - totalOffset >= windowOffset) {
+        $toBeActive = $section;
+      }
     }
+    $("#" + sectionId + "-menu").removeClass("active");
   });
+
+  if (windowOffset + windowHeight == documentHeight) {
+    $toBeActive = $("section").last();
+  }
+
+  if ($toBeActive != null) {
+    $("#" + $toBeActive.attr("id") + "-menu").addClass("active");
+  }
 
 }
